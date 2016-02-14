d7a47a3e915b1b83f63f6b3c92d0011247189437
Ken <ng.ken84@gmail.com>
2016-02-13 23:26:11 -0800

    Update the title of html

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -108,7 +108,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Ken's Picks for a good time</a>
           </div>
         </div>
       </div>
